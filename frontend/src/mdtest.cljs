021871495b3f5f17a5fe4802ffbc9dcdd571ee71
Azamat S. Kalimoulline <turtle@bazon.ru>
2026-08-07 03:02:03 +0300

    frontend: add scratch markdown render check

---
+++ frontend/src/mdtest.cljs
@@ -0,0 +1,20 @@
+;;; focus — ticket tracker
+;;; Copyright (C) 2026 Azamat S. Kalimoulline <turtle@bazon.ru>
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;;
+
+(ns mdtest (:require [focus.markdown :as md]))
+(def url "![](https://plus.unsplash.com/premium_photo-1710965560034-778eedc929ff?q=80&w=715&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)")
+(println "RENDERED:" (md/render-markdown url))

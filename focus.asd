89dcf8f887b57ded145649ecf8df2d0586d0de02
Azamat S. Kalimoulline <turtle@bazon.ru>
2026-08-21 05:06:55 +0300

    version: bump to 0.0.2.2

--- focus.asd
+++ focus.asd
@@ -1,7 +1,7 @@
 (defsystem :focus
   :name "focus"
   :license "GPL-3.0-or-later"
-  :version "0.0.2.1"
+  :version "0.0.2.2"
   :description "Ticket tracker"
   :depends-on (#:clack
                #:clack-handler-wookie
--- frontend/project.clj
+++ frontend/project.clj
@@ -1,4 +1,4 @@
-(defproject focus-frontend "0.0.2.1"
+(defproject focus-frontend "0.0.2.2"
   :description "Focus issue tracker frontend"
   :license {:name "GPL-3.0-or-later"}
   :dependencies [[org.clojure/clojure "1.11.1"]
